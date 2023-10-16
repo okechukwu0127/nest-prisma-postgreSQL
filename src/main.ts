9ac2306e37bfab5397356dc7174226b9e65e0565
Eze Okechukwu <okechukwu0127@gmail.com>
2023-10-16 10:27:17 +0100

    first commit
    first commit

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  //Swagger Documentation Config
+  const config = new DocumentBuilder()
+    .setTitle('Median')
+    .setDescription('The Median API')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
